4822fbed98f47d775e1f49ea1abf6b1071a7404b
arioshka4 <arioshka4@gmail.com>
2024-06-11 02:51:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let a1 = document.getElementById("a1")
+let a2 = document.getElementById("a2")
+let a3 = document.getElementById("a3")
+let a4 = document.getElementById("a4")
+let a5 = document.getElementById("a5")
+
+a1.addEventListener("click", (event) => {
+  alert("І дим батьківщини солодкий.")
+});
+
+a2.addEventListener("click", (event) => {
+  alert("Той, хто не любить своєї країни, нічого любити не може.")
+});
+
+a3.addEventListener("click", (event) => {
+  alert("Кожному мила своя сторона.")
+});
+
+a4.addEventListener("click", (event) => {
+  alert("Усвідомлена любов до свого народу не поєднується з ненавистю до інших.")
+});
+
+a5.addEventListener("click", (event) => {
+  alert("Ті, що виїздять за море, міняють небо, а не душу.")
+});
